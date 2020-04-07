d62cb214c22a0d4da0c51dec1ea41c25ace9303c
Tim Smith <tim.smith.hdg@gmail.com>
2020-04-06 19:57:15 -0700

    logo

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -4,6 +4,17 @@ import styled from 'styled-components';
 const pattern =
   'https://res.cloudinary.com/tihos/image/upload/q_auto/v1585937290/pattern3_btd9kp.png';
 
+const ComingSoonBody = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  background-image: url(${pattern});
+`;
+const Image = styled.img`
+  width: 300px;
+  margin: 0 auto;
+  display: block;
+`;
 const H1 = styled.h1`
   font-size: 8rem;
   font-family: 'blanchcaps_inline', 'Raleway', sans-serif;
@@ -18,25 +29,21 @@ const P = styled.p`
   color: #793817;
   font-size: 2rem;
 `;
-const ComingSoonBody = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  background-image: url(${pattern});
-`;
 const Wrapper = styled.div`
   width: 60vw;
-  padding: 2rem;
-  margin-top: 8rem;
+  padding: 1rem;
+  margin-top: 3.5rem;
   height: fit-content;
   box-shadow: 0 0 0 4px #dfa456, 0 0 0 8px #793817, 0 0 0 12px #ae0e60,
     0 0 0 16px #dfa456;
-  background: rgba(255, 255, 255, 0.25);
+  background: rgba(255, 255, 255, 0.5);
+  border-radius: 20px;
 `;
 
-const ComingSoon = props => (
+const ComingSoon = (props) => (
   <ComingSoonBody>
     <Wrapper>
+      <Image src="images/logo_300.png" alt="The Classic Lunchbox Logo" />
       <H1>Coming Soon</H1>
       <h2>to a farmer's market near you!</h2>
       <P>
